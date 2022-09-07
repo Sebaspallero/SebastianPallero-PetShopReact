497c1feb3c542f37d3a62fa16b288cad4544f74d
Sebastian Pallero <sebastianoria@hotmail.com>
2022-09-07 11:37:21 -0300

    Sexto Commit: Routing

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,71 +1,29 @@
 import React,{useState,useEffect} from 'react'
-import axios from 'axios'
-import { useParams } from 'react-router-dom'
 import ItemDetail from '../ItemDetail/ItemDetail'
-
+import { useParams } from 'react-router-dom'
+import { fetchData } from '../../products'
 
 const ItemDetailContainer = () => {
+  const [product,setProduct]= useState([])
   const {id} = useParams();
-  const productsData = [
-    {
-        id:1,
-        title:'New Zealand - Pollo',
-        price:1350,
-        img_url:"/assets/producto1.jpg"
-    },
-    {
-        id:2,
-        title:'New Zealand - Carne',
-        price:1500,
-        img_url:"/assets/producto2.jpg"
-    },
-    {
-        id:3,
-        title:'New Zealand - Cordero',
-        price:1600,
-        img_url:"/assets/producto3.jpg"
-    },
-    {
-        id:4,
-        title:'New Zealand - Snack Carne',
-        price:1200,
-        img_url:"/assets/producto4.jpg"
-    },
-    {
-        id:5,
-        title:'New Zealand - Snack Ciervo',
-        price:1400,
-        img_url:"/assets/producto5.jpg"
-    },
-    {
-        id:6,
-        title:'New Zealand - Salmon',
-        price:1400,
-        img_url:"/assets/producto6.jpg"
-    },
-    {
-        id:7,
-        title:'New Zealand - Carne Premuim',
-        price:1700,
-        img_url:"/assets/producto7.jpg"
-    },
-    {
-        id:8,
-        title:'New Zealand - Pescado',
-        price:1200,
-        img_url:"/assets/producto8.jpg"
-    }
-]
+
+useEffect(()=>{
+  fetchData
+  .then((res)=>setProduct(res.find( product=>product.id == {id} /* 1 Si se pude pasar un valor numerico */ )))
+  .catch(err=>console.log(err))
+},[])
 
   return (
-    <div>
-      {productsData.filter(product=>product.id==id).map((product)=>(
-        <div key={product.id}>
-          <ItemDetail data={product}/>
-        </div>
-      ))}
-    </div>
-  )
+          <div>
+            <ItemDetail data = {product}/>
+          </div>
+      )
 }
 
 export default ItemDetailContainer
+
+{/* {productsData.filter(product=>product.id==id).map((product)=>(
+        <div key={product.id}>
+          <ItemDetail data={product}/>
+        </div>
+      ))} */}
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,29 +1,22 @@
 import React,{useState,useEffect} from 'react'
 import Item from '../Item/Item'
 import './ItemList.css'
-import axios from 'axios'
+import { fetchData } from '../../products'
 import { Link } from 'react-router-dom'
 
 
 const ItemList = () => {
 
-  //BIN 631549f9e13e6063dc9b4ee0
-
   const [products,setProducts] = useState([]);
 
   useEffect(()=>{
-    axios.get('https://api.jsonbin.io/v3/b/631549f9e13e6063dc9b4ee0',
-     {
-      headers: {"X-Master-Key":"$2b$10$P0mEBR9V68/Q6mKID4J7tO7G1NpEwoSXl.r2v..rdqNM.xj6By4rK"}
-     })
-     .then((res)=> setProducts(res.data.record))
+    fetchData
+       .then((res)=>{
+        return res
+       })
+       .then((resp)=>setProducts(resp))
+       .catch(err=>console.log(err))
   },[])
-
-  
-  /* useEffect(()=>{
-    axios.get('products.json')
-     .then((res)=> setProducts(res.data))
-  },[]) */
 
   return (
     <div className='productsBox'>
